4e6b239a5fea84abe59d683ca708051afc4c83f3
Louise Huuki <lahuuki@gmail.com>
2022-04-13 15:20:13 -0400

    Finish up Supp tables, add tables 3-5 (probe, dye, exposure time) manually

--- code/03_compile_data/compile_supp_tables.R
+++ code/03_compile_data/compile_supp_tables.R
@@ -1,7 +1,8 @@
 library("tidyverse")
 library("xlsx")
 library("here")
-library("sessioninfo")
+
+supp_xlxs <- here("processed-data","03_compile_data","TREG_SupplementaryTables.xlsx")
 
 ## Supplementary Table 1: Numbers of Different Nuclei over Brain Regions
 st1 <- read_csv(here("processed-data","01_find_tregs","supp_tables", "nuclei_counts.csv")) 
@@ -15,14 +16,27 @@ st1_edit <- st1 %>%
   mutate(total = sum(AMY, DLPFC, HPC, NAc, sACC)) %>%
   add_column(`Cell Type Name` = cell_types, .before = 1)
 
+write.xlsx(as.data.frame(st1_edit), file = supp_xlxs, sheetName = "Supp Table 1", 
+           col.names = TRUE, row.names = FALSE, append = FALSE)
+
 ## Supplementary Table 2: Detailed gene metrics
-st2 <- read_csv(here("processed-data","01_find_tregs","supp_tables", "nuclei_counts.csv")) 
+st2 <- read_csv(here("processed-data","01_find_tregs","supp_tables", "gene_metrics2.csv")) %>%
+  select(-1)
+
+write.xlsx(as.data.frame(st2), file = supp_xlxs, sheetName = "Supp Table 2", 
+           col.names = TRUE, row.names = FALSE, append = TRUE)
 
 ## Supplementary Table 3: RNAscope probe combinations and opal dye assignments
+## Added manually
 
 ## Supplementary Table 4: Opal dye dilutions used to label probes in RNAscope
+## Added manually
 
 ## Supplementary Table 5: Scanning protocol exposure times
+## Added manually
 
 # Supplementary Table 6: HALO cell counts
+st6 <- read_csv(here("processed-data","02_analyze_halo","RNAscope_summary.csv"))
 
+write.xlsx(as.data.frame(st6), file = supp_xlxs, sheetName = "Supp Table 6", 
+           col.names = TRUE, row.names = FALSE, append = TRUE)
