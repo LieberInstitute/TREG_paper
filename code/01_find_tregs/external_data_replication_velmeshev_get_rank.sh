539fa90387cb21cd48243c151f865beaee8f6c25
lahuuki <lahuuki@gmail.com>
2023-06-16 10:10:48 -0400

    Use gpu for speed

--- code/01_find_tregs/external_data_replication_velmeshev_get_rank.sh
+++ code/01_find_tregs/external_data_replication_velmeshev_get_rank.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=100G,h_vmem=100G,h_fsize=100G
+#$ -l caracol, mem_free=100G,h_vmem=100G,h_fsize=100G
 #$ -N external_data_replication_velmeshev_get_rank
 #$ -o logs/external_data_replication_velmeshev_get_rank.txt
 #$ -e logs/external_data_replication_velmeshev_get_rank.txt
@@ -23,6 +23,20 @@ module load conda_R/4.3
 
 ## List current modules for reproducibility
 module list
+
+## Use GPU
+USAGE_CUTOFF=10
+NUM_GPUS=1
+
+avail_gpus=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | cut -d " " -f 1 | awk -v usage="$USAGE_CUTOFF" '$1 < usage {print NR - 1}')
+
+#  Simply exit with an error if there are no GPUs left
+if [[ -z $avail_gpus ]]; then
+    echo "No GPUs are available."
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=$(echo "$avail_gpus" | head -n $NUM_GPUS | paste -sd ",")
 
 ## Edit with your job command
 Rscript external_data_replication_velmeshev_get_rank.R $SGE_TASK_ID
